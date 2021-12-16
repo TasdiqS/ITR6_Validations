8c9a0c07d43aa3c7ebf86f452c04f7e9e8faddc7
tasdiqs <tasdiqspace@gmail.com>
2021-12-16 18:01:46 +0530

    added new filess

--- src/test/java/com/validations/testing/ItrValidationsApplicationTests.java
+++ src/test/java/com/validations/testing/ItrValidationsApplicationTests.java
@@ -17,11 +17,10 @@ import com.validations.checking.entities.ITR6Validation;
 
 	
 	@Test
-	void AddC_name() {
+	void validation() {
 
 		ITR6Validation val = new ITR6Validation();
 
-		
 		val.setC_name("COMPANY");
 		val.setAddress("MUMBAI WADALA 400037");
 		val.setZipCode("400037");
@@ -30,11 +29,15 @@ import com.validations.checking.entities.ITR6Validation;
 		val.setPanId("EDCBA1234F");
 		val.setPhNo("9745000000");
 		val.setcIN("L12345AB1234ABC123456");
+		val.setdIN("12345678");
 		val.setDate("12/01/2021");
 		val.setCountry("India");
+		val.setAmount("20000");
 		val.setAccountNumber("123456789012345690");
 		val.setBsrCode("12345AB");
 		val.setSerialNumberOfChallan("123AB");
+		val.setSebiRegNumber("12345678");
+		val.setCountryCode("+91");
 		
 		this.valrepo.save(val);
 		//boolean result = 
